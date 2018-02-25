49f18d2fd7877c83f91fdcfd359640b29093bb3c
Victor <vsomotun@gmail.com>
2018-02-24 22:52:43 -0500

    Location Milestone Reached
    The geofire Location uploader has been reached and finished.

--- app/src/main/java/installedheights/bigbrother20/MainActivity.java
+++ app/src/main/java/installedheights/bigbrother20/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
 import com.firebase.geofire.LocationCallback;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,27 +24,50 @@ public class MainActivity extends AppCompatActivity {
         final DatabaseReference ref = FirebaseDatabase.getInstance().getReference("geofire");
         final GeoFire geoFire = new GeoFire(ref);
         Button button2 = (Button) findViewById(R.id.button2);
+
+
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 GeoLocation gel = new GeoLocation(37.7853889, -122.4056973);
                 geoFire.setLocation(ref.push().getKey(), gel, new GeoFire.CompletionListener() {
+
                     @Override
                     public void onComplete(String key, DatabaseError error) {
                         Log.v("BigBro", "Update Map");
-                        geoFire.getLocation("BigBro", new LocationCallback() {
-                            @Override
-                            public void onLocationResult(String key, GeoLocation location) {
 
-                            }
-
-                            @Override
-                            public void onCancelled(DatabaseError databaseError) {
-
-                            }
-                        });
                     }
                 });
+            }
+        });
+
+
+        GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(37.7832, -122.4056), 0.6);
+
+        geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
+            @Override
+            public void onKeyEntered(String key, GeoLocation location) {
+                System.out.println(String.format("Key %s entered the search area at [%f,%f]", key, location.latitude, location.longitude));
+            }
+
+            @Override
+            public void onKeyExited(String key) {
+                System.out.println(String.format("Key %s is no longer in the search area", key));
+            }
+
+            @Override
+            public void onKeyMoved(String key, GeoLocation location) {
+                System.out.println(String.format("Key %s moved within the search area to [%f,%f]", key, location.latitude, location.longitude));
+            }
+
+            @Override
+            public void onGeoQueryReady() {
+                System.out.println("All initial data has been loaded and events have been fired!");
+            }
+
+            @Override
+            public void onGeoQueryError(DatabaseError error) {
+                System.err.println("There was an error with this query: " + error);
             }
         });
     }
