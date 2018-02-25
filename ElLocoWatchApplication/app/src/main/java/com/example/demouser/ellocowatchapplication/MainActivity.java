eec6b70165b94e5275815241696f16ae3ad7f87b
ChrisI <chrisingram.ingram1@gmail.com>
2018-02-25 04:14:19 -0500

    FrontEnd
    Just link at the moment

--- ElLocoWatchApplication/app/src/main/java/com/example/demouser/ellocowatchapplication/MainActivity.java
+++ ElLocoWatchApplication/app/src/main/java/com/example/demouser/ellocowatchapplication/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.demouser.ellocowatchapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.planets_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        //spinner.setOnItemSelectedListener(this);
+
     }
 }
