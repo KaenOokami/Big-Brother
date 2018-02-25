7bd062dae78d1ee7cc24cd8566a7af6d43caf105
Victor <vsomotun@gmail.com>
2018-02-24 20:03:25 -0500

    Big Brother Milestone Pings are active on Firebase

--- app/src/main/java/installedheights/bigbrother20/MainActivity.java
+++ app/src/main/java/installedheights/bigbrother20/MainActivity.java
@@ -2,6 +2,13 @@ package installedheights.bigbrother20;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("geofire");
+        final GeoFire geoFire = new GeoFire(ref);
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                geoFire.setLocation("firebase-hq", new GeoLocation(37.7853889, -122.4056973));
+            }
+        });
     }
 }
